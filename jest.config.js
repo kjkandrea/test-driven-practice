fecf238fa554fed051ebb5f108d5e54091cbb95c
jongkeun-kim <jongkeun@nhn-commerce.com>
2021-11-24 12:41:05 +0900

    Add component test sample

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
+
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|gif|ttf|woff|woff2)$': "jest-transform-stub",
+  },
+  moduleNameMapper: {
+    '^.+.(css|styl|less|sass|scss|png|jpg|gif|ttf|woff|woff2)$': "jest-transform-stub",
+  },
 };
