ef4d4c6074c0d9530d8f864f600cfb0c5b078266
kjkandrea <kjkandrea@gmail.com>
2021-11-27 18:50:18 +0900

    Refactor list test

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -4,12 +4,14 @@ import List from './List';
 import tasks from '../../../fixtures/tasks';
 
 describe('List', () => {
+  const  renderList = tasks => render(
+    (<List tasks={tasks} />),
+  );
+
   // 테스트가 주어지는 상황
   context('with tasks', () => {
     it('render tasks', () => {
-      const { container } = render(
-        (<List tasks={tasks} />),
-      );
+      const { container } = renderList(tasks)
 
       expect(container).toHaveTextContent('Where the hell my homies went?')
       expect(container).toHaveTextContent('Looking around like where my phone?')
@@ -18,9 +20,7 @@ describe('List', () => {
     it('render "완료" button to delete a task', () => {
       const handleClick = jest.fn()
 
-      const { getAllByText } = render(
-        (<List tasks={tasks} />),
-      );
+      const { getAllByText } = renderList(tasks)
 
       const buttons = getAllByText('완료')
 
@@ -33,9 +33,7 @@ describe('List', () => {
   // 테스크가 주어지지 않는 상황
   context('without tasks', () => {
     it('render no task message', () => {
-      const { container } = render(
-        (<List tasks={[]} />),
-      );
+      const { container } = renderList([])
 
       expect(container).toHaveTextContent('Dobby is free.')
     });
