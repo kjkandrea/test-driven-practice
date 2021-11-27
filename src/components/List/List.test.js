41ff61e56fc17b3c85fd7c1f7d938ad7f554a8fd
kjkandrea <kjkandrea@gmail.com>
2021-11-27 18:16:24 +0900

    Add list BDD test : red

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -4,12 +4,26 @@ import List from './List';
 import tasks from '../../../fixtures/tasks';
 
 describe('List', () => {
-  it('render tasks', () => {
-    const { container } = render(
-      (<List tasks={tasks} />),
-    );
+  // 테스트가 주어지는 상황
+  context('with tasks', () => {
+    it('render tasks', () => {
+      const { container } = render(
+        (<List tasks={tasks} />),
+      );
 
-    expect(container).toHaveTextContent('Where the hell my homies went?')
-    expect(container).toHaveTextContent('Looking around like where my phone?')
-  });
+      expect(container).toHaveTextContent('Where the hell my homies went?')
+      expect(container).toHaveTextContent('Looking around like where my phone?')
+    });
+  })
+
+  // 테스크가 주어지지 않는 상황
+  context('without tasks', () => {
+    it('render no task message', () => {
+      const { container } = render(
+        (<List tasks={[]} />),
+      );
+
+      expect(container).toHaveTextContent('Dobby is free.')
+    });
+  })
 });
