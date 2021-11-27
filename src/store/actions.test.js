b7bb5e58f07b206c271e9e30ca2faee63942e171
kjkandrea <kjkandrea@gmail.com>
2021-11-27 18:06:15 +0900

    Add reducer test

---
+++ src/store/actions.test.js
@@ -0,0 +1,15 @@
+import reducer from './reducer';
+import { setTasks } from './actions';
+import tasks from '../../fixtures/tasks';
+
+describe('reducer', () => {
+  describe('setTasks', () => {
+    it('change tasks array', () => {
+      const state = reducer({
+        tasks: []
+      }, setTasks(tasks))
+
+      expect(state.tasks).not.toHaveLength(0)
+    })
+  })
+})
